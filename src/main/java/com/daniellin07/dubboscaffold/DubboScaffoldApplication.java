330532d6dc1bc42c1f9f476f021da44ed4498c5d
DanielLin07 <553925608@qq.com>
2019-09-01 18:06:45 +0800

    [minor] 添加Dubbo依赖

--- src/main/java/com/daniellin07/dubboscaffold/DubboScaffoldApplication.java
+++ src/main/java/com/daniellin07/dubboscaffold/DubboScaffoldApplication.java
@@ -3,6 +3,12 @@ package com.daniellin07.dubboscaffold;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * 应用入口
+ *
+ * @author DanielLin07
+ * @date 2019/9/1 17:52
+ */
 @SpringBootApplication
 public class DubboScaffoldApplication {
 
